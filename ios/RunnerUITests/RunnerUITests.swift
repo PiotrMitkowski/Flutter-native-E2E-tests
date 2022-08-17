342fce121d85cb539ce5026972e16342adc3ec95
Piotr Mitkowski <piotr.mitkowski1@gmail.com>
2022-08-17 13:15:24 +0200

    Implemented geolocation page test on iOS

--- ios/RunnerUITests/RunnerUITests.swift
+++ ios/RunnerUITests/RunnerUITests.swift
@@ -30,10 +30,10 @@ class RunnerUITests: XCTestCase {
             allPhotoPermission.tap()
             return true
         }
-        addUIInterruptionMonitor(withDescription: "Geolocation permission popup") { [weak self] element in
-            let whileUsingPermission = element.buttons["While using the app"]
+        addUIInterruptionMonitor(withDescription: "Location Services") { element in
+            let whileUsingPermission = element.buttons["Allow While Using App"]
             if !whileUsingPermission.waitForExistence(timeout: systemPopupWaitingTime) {
-                print(self?.app.debugDescription ?? "Self detached")
+                print(self.app.debugDescription)
                 XCTFail("Failed due to not able to find whileUsingPermission button with \(systemPopupWaitingTime) seconds")
 
             }
@@ -72,16 +72,29 @@ class RunnerUITests: XCTestCase {
     func testGettingCurrentLocation() throws {
         let geolocationPageButton = app.buttons["Geolocation test"]
         if !geolocationPageButton.waitForExistence(timeout: kElementWaitingTime) {
-            XCTFail("Failed due to not able to find counter button with \(kElementWaitingTime) seconds")
+            XCTFail("Failed due to not able to find geolocation page button with \(kElementWaitingTime) seconds")
         }
         XCTAssertTrue(geolocationPageButton.exists)
         geolocationPageButton.tap()
         
-//        let counterLabelPredicate = NSPredicate(format: "label ==[c] %@", "Button tapped 1 time.")
-//        let counterLabel = app.staticTexts.element(matching: counterLabelPredicate)
-//        if !counterLabel.waitForExistence(timeout: kElementWaitingTime) {
-//            XCTFail("Failed due to not able to find counter label with \(kElementWaitingTime) seconds")
-//        }
-//        XCTAssert(counterLabel.exists)
+        let currentLocationButton = app.buttons["Get current location"]
+        if !currentLocationButton.waitForExistence(timeout: kElementWaitingTime) {
+            XCTFail("Failed due to not able to find current location button with \(kElementWaitingTime) seconds")
+        }
+        XCTAssert(currentLocationButton.exists)
+        currentLocationButton.tap()
+        
+        // There is a known bug where the permission popups interruption won't get fired until a tap
+        // happened in the app. We expect a permission popup so we do a tap here.
+        let exp = expectation(description: "Test after 2 seconds")
+        let _ = XCTWaiter.wait(for: [exp], timeout: 2.0)
+        app.tap()
+        
+        let currentPositionLabelPredicate = NSPredicate(format: "label ==[c] %@", "Current location: 45.47655, -73.61355")
+        let currentPositionLabel = app.staticTexts.element(matching: currentPositionLabelPredicate)
+        if !currentPositionLabel.waitForExistence(timeout: systemPopupWaitingTime) {
+            XCTFail("Failed due to not able to find current position label with \(kElementWaitingTime) seconds")
+        }
+        XCTAssert(currentPositionLabel.exists)
     }
 }
